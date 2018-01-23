cd4db8c2b5b50d118445111ee514abb60a35b7ab
Tom Trinter <tttrinter@gmail.com>
2018-01-23 16:15:38 -0500

    Updated to point to DI AWS

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,6 +1,7 @@
 from Scripts.service import run_topic_continuous
 
 run_topic_continuous(topic_id=9,
-                     s3_bucket='aqtwitter',
-                     s3_path='tweets/Life Events/Graduation/',
-                     tweet_count=1000000)
+                     s3_bucket='di-thrivent',
+                     s3_path='twitter/Life Events/Graduation/',
+                     tweet_count=10)
+

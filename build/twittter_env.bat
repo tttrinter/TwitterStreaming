5d192397a1ff47edc7128578c2f29a4dc4f1ec76
Ben Wood <ben@datainsightsllc.com>
2018-02-25 19:02:59 -0800

    added sqlalchemy to build and gitignore

--- build/twittter_env.bat
+++ build/twittter_env.bat
@@ -1,5 +1,6 @@
+setx path "%path%;c:\Users\%USERNAME%\Anaconda3\Scripts\"
 conda create -n twitter_env python=3.6 --yes
-source activate twitter_env
+activate twitter_env
 pip install tweepy
 pip install boto3
 conda install -y pandas
@@ -9,3 +10,4 @@ conda install -y scikit-learn
 conda install -y nltk
 conda install -y pytables
 conda install -y snappy
+conda install -y sqlalchemy

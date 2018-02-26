62765000f557b1063561a1261c452a9972eab16f
Ben Wood <ben@datainsightsllc.com>
2018-02-25 18:02:24 -0800

    add a build script with conda env and all package installs

---
+++ build/twittter_env.bat
@@ -0,0 +1,11 @@
+conda create -n twitter_env python=3.6 --yes
+source activate twitter_env
+pip install tweepy
+pip install boto3
+conda install -y pandas
+pip install psycopg2
+conda install -y numba
+conda install -y scikit-learn
+conda install -y nltk
+conda install -y pytables
+conda install -y snappy
